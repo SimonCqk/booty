7b93a21cdcee1f6c5f68ee82d063418aa4839bf8
Cqk <cqk0100@gmail.com>
2017-11-12 23:18:46 +0800

    initial repository.

---
+++ README.md
@@ -0,0 +1,2 @@
+# ThreadPool
+A tiny thread pool based on C++11/14.
---
+++ ThreadPool.h
@@ -0,0 +1,11 @@
+#pragma once
+#pragma once
+#ifndef _THREAD_POOL_H
+#define _THREAD_POOL_H
+
+class ThreadPool {
+	explicit ThreadPool(const size_t& thread_num);
+
+};
+
+#endif // !_THREAD_POOL_H
