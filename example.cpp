9e154133195cdca99518d150f0b2faa6c986f7df
Cqk <cqk0100@gmail.com>
2017-11-16 22:59:05 +0800

    do more performance test. [ join() may be a noise. ]

--- example.cpp
+++ example.cpp
@@ -9,29 +9,47 @@ using namespace thread_pool;
 int main()
 {
 	using namespace std::literals;
-	ThreadPool pool(5);
+	using namespace std::chrono;
+	auto start1 = system_clock::now();
+	std::vector<int> answers;
+	for (int i = 0; i < 1000; ++i) {
+		std::thread th([i,&answers] {
+			std::cout << " process... " << i;
+			std::this_thread::sleep_for(100ms);
+			std::cout << " still process... " << i;
+			answers.push_back(i*i);
+		});
+		th.join();
+	}
+	for (auto& ans : answers) {
+		std::cout << ans << ' ';
+	}
+	auto end1 = system_clock::now();
+	auto start2= system_clock::now();
+	ThreadPool pool(10);
 	std::vector< std::future<int> > results;
-	for (int i = 0; i < 50; ++i) {
-		if (!(i % 10) && i != 0) {
-			pool.pause();
-			std::cout << "Being paused..." << std::endl;
-			std::this_thread::sleep_for(10s);
-			pool.unpause();
-			std::cout << "Being unpaused..." << std::endl;
-		}
+	for (int i = 0; i < 1000; ++i) {
 		results.emplace_back(
 			pool.submitTask([i] {
-			std::cout << "hello " << i << std::endl;
-			std::this_thread::sleep_for(1s);
-			std::cout << "world " << i << std::endl;
+			std::cout << " process... " << i;
+			std::this_thread::sleep_for(100ms);
+			std::cout << " still process... " << i;
 			return i*i;
 		})
 		);
 	}
-
 	for (auto && result : results)
 		std::cout << result.get() << ' ';
 	std::cout << std::endl;
-
+	auto end2=system_clock::now();
+    
+	auto dur1 = duration_cast<microseconds>(end1 - start1);
+	auto dur2 = duration_cast<microseconds>(end2 - start2);
+	std::cout << "Test one took "
+		<< double(dur1.count()) * microseconds::period::num / microseconds::period::den
+		<< "s" << std::endl;
+	std::cout << "Test two took "
+		<< double(dur2.count()) * microseconds::period::num / microseconds::period::den
+		<< "s" << std::endl;
 	return 0;
 }
