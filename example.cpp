5fcc39f2f6289c4428b9a4361314b0aa0a4f34d5
Cqk <cqk0100@gmail.com>
2017-11-29 20:24:30 +0800

    update new test code and new test result image. ( the performance gap is so shocking and need to be confirmed... )

--- example.cpp
+++ example.cpp
@@ -8,22 +8,22 @@ using namespace thread_pool;
 
 int main()
 {
+	std::mutex mtx;
 	using namespace std::literals;
 	using namespace std::chrono;
 	auto start1 = system_clock::now();
 	std::vector<std::thread> threads;
 	for (int i = 0; i < 1000; ++i) {
-		threads.emplace_back([i] {
-			std::mutex mtx;
+		threads.emplace_back([i, &mtx] {
 			{
 				std::lock_guard<std::mutex> lock(mtx);
 				std::cout << " process... " << i;
 			}
 			int test;
-			for (int j = 0; j < 10000; ++j) {
+			for (int j = 0; j < 1000000; ++j) {
 				test = j * 100 / 123;
 			}
-			for (int j = 0; j < 10000; ++j) {
+			for (int j = 0; j < 1000000; ++j) {
 				test *= j * 123 / 100;
 			}
 			{
@@ -38,38 +38,37 @@ int main()
 	auto end1 = system_clock::now();
 	auto start2 = system_clock::now();
 	ThreadPool pool(4);
-	std::vector< std::future<int> > results;
 	for (int i = 0; i < 1000; ++i) {
-		results.emplace_back(
-			pool.submitTask([i] {
-			std::mutex mtx;
+		pool.submitTask([i, &mtx] {
 			{
 				std::lock_guard<std::mutex> lock(mtx);
 				std::cout << " process... " << i;
 			}
 			int test;
-			for (int j = 0; j < 10000; ++j) {
+			for (int j = 0; j < 1000000; ++j) {
 				test = j * 100 / 123;
 			}
-			for (int j = 0; j < 10000; ++j) {
+			for (int j = 0; j < 1000000; ++j) {
 				test *= j * 123 / 100;
 			}
-			return (test*test) % 1000;
-		})
-		);
+			{
+				std::lock_guard<std::mutex> lock(mtx);
+				std::cout << (test*test) % 1000 << ' ';
+			}
+		});
 	}
-	for (auto&& result : results)
-		std::cout << result.get() << ' ';
 	auto end2 = system_clock::now();
 	std::cout << std::endl;
 	auto dur1 = duration_cast<microseconds>(end1 - start1);
 	auto dur2 = duration_cast<microseconds>(end2 - start2);
-	std::cout << "Test one took "
-		<< double(dur1.count()) * microseconds::period::num / microseconds::period::den
-		<< "s" << std::endl;
-	std::cout << "Test two took "
-		<< double(dur2.count()) * microseconds::period::num / microseconds::period::den
-		<< "s" << std::endl;
-
+	{
+		std::lock_guard<std::mutex> lock(mtx);
+		std::cout << "Test one took "
+			<< double(dur1.count()) * microseconds::period::num / microseconds::period::den
+			<< "s" << std::endl;
+		std::cout << "Test two took "
+			<< double(dur2.count()) * microseconds::period::num / microseconds::period::den
+			<< "s" << std::endl;
+	}
 	return 0;
 }
