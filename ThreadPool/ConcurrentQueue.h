53dd9b3c56cfaa46af6f05512d1e4609cb81f287
Cqk <cqk0100@gmail.com>
2017-12-06 10:48:37 +0800

    add more work on concurrent queue.

--- ThreadPool/ConcurrentQueue.h
+++ ThreadPool/ConcurrentQueue.h
@@ -2,6 +2,7 @@
 #ifndef _CONCURRENT_QUEUE_H
 #define _CONCURRENT_QUEUE_H
 
+#include<memory>
 #include"./detail/ConcurrentQueue_impl.h"
 
 namespace concurrentlib {
@@ -10,7 +11,7 @@ namespace concurrentlib {
 		ConcurrentQueue(const size_t max_elements);
 
 	private:
-
+		std::shared_ptr<ConcurrentQueue_impl> queue;
 	};
 }
 
--- ThreadPool/detail/ConcurrentQueue_impl.h
+++ ThreadPool/detail/ConcurrentQueue_impl.h
@@ -2,22 +2,89 @@
 #ifndef _CONCURRENT_QUEUE_IMPL_H
 #define _CONCURRENT_QUEUE_IMPL_H
 
+#include<array>
 #include<thread>
 #include<atomic>
 #include<condition_variable>
 
 namespace concurrentlib {
 
+	template<typename T>
 	class ConcurrentQueue_impl {
-		//define some type traits & alias.
+		struct ListNode;
+		struct ContLinkedList;
+
+		//define some type traits and constants.
 		using index_t = std::size_t;
+		using asize_t = std::atomic_size_t;
+		using abool = std::atomic_bool;
+		using aListNode_p = std::atomic<ListNode*>;
+		using aContLinkedList = std::atomic<ContLinkedList>;
+
+		constexpr size_t SUB_QUEUE_NUM = 8;
+
+		// define free-list structure.
+		struct ListNode
+		{
+			T data;
+			std::atomic<ListNode*> next;
+			ListNode() {
+				next.store(nullptr, std::memory_order_relaxed);
+			}
+
+			template<typename Ty>
+			ListNode(Ty&& val)
+				:data(std::forward<Ty>(val)) {
+				next.store(nullptr, std::memory_order_relaxed);
+			}
+		};
+
+		struct ContLinkedList
+		{
+			aListNode_p head;
+			aListNode_p tail;
+			ContLinkedList() {
+				head.store(nullptr);
+				tail.store(head.load(std::memory_order_relaxed), std::memory_order_relaxed);  // init: head = tail
+			}
+		};
 
 	public:
-		ConcurrentQueue_impl(const size_t& max_elements);
-	private:
-		std::condition_variable cond_empty;
+		ConcurrentQueue_impl();
+		ConcurrentQueue_impl(const size_t& num_elements=100);
 
+
+
+		size_t size() const;
+		bool empty() const;
+
+		~ConcurrentQueue_impl();
+
+		// forbid copy ctor & copy operator.
+		ConcurrentQueue_impl(const ConcurrentQueue_impl& queue) = delete;
+		ConcurrentQueue_impl& operator=(const ConcurrentQueue_impl& queue) = delete;
+
+	private:
+		size_t _choose();
+
+
+		std::condition_variable cond_empty;
+		std::array<ContLinkedList, SUB_QUEUE_NUM> sub_queues;
+		asize_t _size;
 	};
+
+	template<typename T>
+	inline size_t ConcurrentQueue_impl<T>::size() const
+	{
+		return _size.load();
+	}
+
+	template<typename T>
+	inline bool ConcurrentQueue_impl<T>::empty() const
+	{
+		return _size.load() == 0;
+	}
+
 }
 
 #endif // !_CONCURRENT_QUEUE_IMPL_H
