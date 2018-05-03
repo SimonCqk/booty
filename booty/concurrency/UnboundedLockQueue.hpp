09540fe76611930f43e850bfc130c3a4fcd9abf5
Cqk <cqk0100@gmail.com>
2018-05-03 18:42:27 +0800

    add some neccessary comments.

--- booty/concurrency/UnboundedLockQueue.hpp
+++ booty/concurrency/UnboundedLockQueue.hpp
@@ -14,24 +14,39 @@ namespace booty {
 	using AtomicSize = std::atomic<size_t>;
 
 	namespace concurrency {
+
+		/// UnboundedLockQueue provides thread-safe operations with STL lock.
+		/// enqueue/dequeue never blocks. If neccessary, block measures should
+		/// be adopted by caller/invoker.
+
 		template<typename T>
 		class UnboundedLockQueue {
 		public:
 			UnboundedLockQueue()
 				:size_(0) {}
 
+			/* enqueue one element(lvalue). */
 			void enqueue(const T& ele) {
 				std::lock_guard<std::mutex> lock(this->queue_mtx_);
 				queue_.push(ele);
 				size_.fetch_add(1, std::memory_order_release);
 			}
 
+			/*
+			  enqueue one element(rvalue).
+			*/
 			void enqueue(T&& ele) {
 				std::lock_guard<std::mutex> lock(this->queue_mtx_);
 				queue_.emplace(std::move(ele));
 				size_.fetch_add(1, std::memory_order_release);
 			}
 
+			/*
+			  dequeue one element, pass by reference.
+			  return by a local value is not exception-safe, and when queue is empty,
+			  it just throw an error by std::queue, but never block to wait for producer,
+			  as it mentioned above: block measures should be adopted by caller/invoker.
+			*/
 			void dequeue(T& recv) {
 				std::lock_guard<std::mutex> lock(this->queue_mtx_);
 				recv = std::move(queue_.front());
@@ -39,21 +54,25 @@ namespace booty {
 				size_.fetch_sub(1, std::memory_order_release);
 			}
 
+			/* get size of queue */
 			size_t size() {
 				return this->size_.load(std::memory_order_relaxed);
 			}
 
+			/* judge if queue is empty */
 			bool empty() {
 				return this->size_.load(std::memory_order_relaxed) == 0;
 			}
 
-			// forbid copy ctor and copy operator
+			/* forbid copy ctor and copy operator */
 			UnboundedLockQueue(const UnboundedLockQueue&) = delete;
 			UnboundedLockQueue& operator=(const UnboundedLockQueue&) = delete;
 
 		private:
 			std::queue<T> queue_;
 			std::mutex queue_mtx_;
+			// use atomic var to avoid construct and destruct a lock everytime when
+			// size() and empty() was invoked.
 			AtomicSize size_;
 		};
 	}
--- booty/concurrency/UnboundedQueue.hpp
+++ booty/concurrency/UnboundedQueue.hpp
@@ -2,12 +2,14 @@
  * This is a derivative snippet of Facebook::folly, under Apache Lisence.
  * Indention:
  * - Recurrent the design idea of seniors and rewrite some details to adapt
- *   personal considerations.
+ *   personal considerations as components of booty.
  *
  * @Simoncqk - 2018.05.02
  */
 #ifndef BOOTY_CONCURRENCY_UNBOUNDEDQUEUE_H
 #define BOOTY_CONCURRENCY_UNBOUNDEDQUEUE_H
+
+
 
 
 #endif // !BOOTY_CONCURRENCY_UNBOUNDEDQUEUE_H
