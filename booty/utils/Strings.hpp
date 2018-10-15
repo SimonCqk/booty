e6866a43f56ec59d2f42ac4e11e7b6c1a5125a0c
Cqk <cqk0100@gmail.com>
2018-10-15 19:46:39 +0800

    format Strings.hpp

--- booty/utils/Strings.hpp
+++ booty/utils/Strings.hpp
@@ -5,24 +5,24 @@
 #ifndef BOOTY_UTILS_STRINGS_HPP
 #define BOOTY_UTILS_STRINGS_HPP
 
-#include<string>
-#include<vector>
+#include <string>
+#include <vector>
 
-namespace booty{
+namespace booty {
 
-    /// Split string into words by given separator.
-    std::vector<std::string> Split(const std::string& str,const char& sep='\n'){
-        if (str.size() <= 1) return {str};
-        std::vector<std::string> res;
-        auto beg = str.cbegin(), end = str.cend(),mover=str.cbegin();
-        while(mover!=end){
-            while (mover != end && (*mover) != sep) ++mover;
-            res.emplace_back(beg, mover);
-            while ((*mover) == sep) ++mover;  // skip redundent seperators.
-            beg = mover;
-        }
-        return res;
-    }
-}
+	/// Split string into words by given separator.
+	std::vector<std::string> Split(const std::string& str, const char& sep = '\n') {
+		if (str.size() <= 1) return { str };
+		std::vector<std::string> res;
+		auto beg = str.cbegin(), end = str.cend(), mover = str.cbegin();
+		while (mover != end) {
+			while (mover != end && (*mover) != sep) ++mover;
+			res.emplace_back(beg, mover);
+			while ((*mover) == sep) ++mover;  // skip redundent seperators.
+			beg = mover;
+		}
+		return res;
+	}
+}  // namespace booty
 
 #endif
