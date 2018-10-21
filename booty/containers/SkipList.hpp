344daf3c2fdf70818d692c4ae3bd286f7e05f6df
Cqk <cqk0100@gmail.com>
2018-10-21 20:54:17 +0800

    change template instantiation way of SkipList.

--- booty/containers/SkipList.hpp
+++ booty/containers/SkipList.hpp
@@ -3,81 +3,85 @@
 
 #include <memory>
 #include<atomic>
+#include<functional>
 
 namespace booty {
 
-namespace containers {
+	namespace containers {
 
-template <typename Key, class Comparator>
-class SkipList {
+		template <
+			typename Key,
+			class Comparator = std::less<typename Key>,
+			int kMaxHeight = 16,
+			int kBranching = 8
+		>
+			class SkipList {
 
-    constexpr static int kMaxHeight = 16;
-    constexpr static int kBranching = 8 ;
-    struct Node;
+			static_assert(kMaxHeight >= 16 && kMaxHeight < 64, "kMaxHeight must be in range if [16, 64)");
+			static_assert(kMaxHeight & 0x01 == 0, "kMaxHeight must be power of 2");
 
-   public:
-    explicit SkipList(Comparator cmp);
+			struct Node;
 
-    void Insert(const Key& key);
+			public:
+				void Insert(const Key& key);
 
-    void Insert(Key&& key);
+				void Insert(Key&& key);
 
-    bool Contains(const Key& key);
+				bool Contains(const Key& key);
 
-    class Iterator {
-       public:
-        explicit Iterator(
-            const std::shared_ptr<SkipList<Key, Comparator>>& list);
+				class Iterator {
+				public:
+					explicit Iterator(
+						const std::shared_ptr<SkipList<Key, Comparator>>& list);
 
-        bool Valid() const;
+					bool Valid() const;
 
-        const Key& Key() const;
+					const Key& Key() const;
 
-        void Next();
+					void Next();
 
-        void Prev();
+					void Prev();
 
-        void Seek(const Key& target);
+					void Seek(const Key& target);
 
-        void SeekToFirst();
+					void SeekToFirst();
 
-        void SeekToLast();
+					void SeekToLast();
 
-        private:
-         const std::shared_ptr<SkipList> list_;
-         std::shared_ptr<Node> node_;
-    };
+				private:
+					const std::shared_ptr<SkipList> list_;
+					std::shared_ptr<Node> node_;
+				};
 
-    SkipList(const SkipList&) = delete;
-    SkipList& operator=(const SkipList&) = delete;
+				SkipList(const SkipList&) = delete;
+				SkipList& operator=(const SkipList&) = delete;
 
-   private:
-    const Comparator compare_;
-    std::shared_ptr<Node> head_;
-    std::atomic<int> maxHeight_;
+			private:
+				std::shared_ptr<Node> head_;
+				std::atomic<int> maxHeight_;
 
-    inline int getMaxHeight() const {
-        return maxHeight_.load(std::memory_order_acquire);
-     }
+				inline int getMaxHeight() const {
+					return maxHeight_.load(std::memory_order_acquire);
+				}
 
-     std::shared_ptr<Node> newNode(const Key& key, int height);
-     int randomHeight();
+				std::shared_ptr<Node> newNode(const Key& key, int height);
+				int randomHeight();
 
-     inline bool isEqual(const Key& lhs,const Key& rhs) const {
-         return compare_(lhs, rhs) == 0;
-     }
+				inline bool isEqual(const Key& lhs, const Key& rhs) const {
+					return compare_(lhs, rhs) == 0;
+				}
 
-     bool keyIsAfterNode(const Key& key,
-                         const std::shared_ptr<Node>& node) const;
+				bool keyIsAfterNode(const Key& key,
+					const std::shared_ptr<Node>& node) const;
 
-     std::shared_ptr<Node> findGreaterOrEqual(
-         const Key& key, const std::shared_ptr<Node*>& prev) const;
+				std::shared_ptr<Node> findGreaterOrEqual(
+					const Key& key, const std::shared_ptr<Node*>& prev) const;
 
-     std::shared_ptr<Node> findLessThan(const Key& key) const;
+				std::shared_ptr<Node> findLessThan(const Key& key) const;
 
-     std::shared_ptr<Node> findLast() const;
-};
-}  // namespace containers
+				std::shared_ptr<Node> findLast() const;
+		};
+	}  // namespace containers
 
 }  // namespace booty
 
