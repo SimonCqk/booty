b69a1ed59bbc8a1929ad4c812bece942ebd7d279
Simoncqk <cqk0100@gmail.com>
2019-02-12 16:57:50 +0800

    add two help string function.

--- booty/utils/Strings.hpp
+++ booty/utils/Strings.hpp
@@ -10,19 +10,46 @@
 
 namespace booty {
 
-	/// Split string into words by given separator.
-	std::vector<std::string> Split(const std::string& str, const char& sep = '\n') {
-		if (str.size() <= 1) return { str };
-		std::vector<std::string> res;
-		auto beg = str.cbegin(), end = str.cend(), mover = str.cbegin();
-		while (mover != end) {
-			while (mover != end && (*mover) != sep) ++mover;
-			res.emplace_back(beg, mover);
-			while ((*mover) == sep) ++mover;  // skip redundent seperators.
-			beg = mover;
+	namespace strings {
+
+		/// Split string into words by given separator.
+		std::vector<std::string> Split(const std::string& str, const char& sep = '\n') {
+			if (str.size() <= 1) return { str };
+			std::vector<std::string> res;
+			auto beg = str.cbegin(), end = str.cend(), mover = str.cbegin();
+			while (mover != end) {
+				while (mover != end && (*mover) != sep) ++mover;
+				res.emplace_back(beg, mover);
+				while ((*mover) == sep) ++mover;  // skip redundent seperators.
+				beg = mover;
+			}
+			return res;
 		}
-		return res;
-	}
+
+		/// Judge whether string starts with the given substring.
+		bool StartWith(const std::string& str, const std::string& sub) {
+			if (str.size() < sub.size()) return false;
+			for (auto beg = str.cbegin(), end = str.cend(), sbeg = sub.cbegin(), send = sub.cend();
+				beg != end; ++beg, ++sbeg) {
+				if (sbeg == send)break;
+				if ((*beg) != (*sbeg)) return false;
+			}
+			return true;
+		}
+
+		/// Judge whether string ends with the given substring.
+		bool EndWith(const std::string& str, const std::string& sub) {
+			if (str.size() < sub.size()) return false;
+			for (auto beg = str.crbegin(), end = str.crend(), sbeg = sub.crbegin(), send = sub.crend();
+				beg != end; ++beg, ++sbeg) {
+				if (sbeg == send)break;
+				if ((*beg) != (*sbeg)) return false;
+			}
+			return true;
+		}
+
+	} // namespace strings
+
 }  // namespace booty
 
 #endif
